8ff583a834cfe21191512718dc513a8f10eaf98d
williankazu <oniguiri40@gmail.com>
2024-11-04 21:12:44 -0300

    segundo-projeto-netflix

---
+++ script.js
@@ -0,0 +1,30 @@
+let botaoSom = document.querySelector(".botao-som")
+let video = document.querySelector(".video")
+let botao = document.querySelector(".link-info")
+let modal = document.querySelector(".modal")
+let audio = document.querySelector(".audio")
+
+botaoSom.addEventListener("click", ligaSom)
+
+function ligaSom(){
+    video.muted = !video.muted
+}
+
+
+
+botao.addEventListener("click",  mostrarModal)
+modal.addEventListener("click", esconderModal)
+
+function mostrarModal(){
+    modal.style.display = "block"
+}
+
+function esconderModal(){
+    modal.style.display = "none"
+}
+
+window.addEventListener("click", tocarAudio)
+
+function tocarAudio(){
+    audio.play()
+}
